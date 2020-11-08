7e10c40b472a12ccbc3104fc5160e3e02d72eece
songhee <thdgml5537@gmail.com>
2020-11-08 01:33:40 -0500

    complete write file with rendered data

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const questions = [
@@ -74,11 +73,16 @@ const userOfficeNumber = [
 
 const teamMember = [];
 
+function output(divs){
+    fs.writeFileSync(outputPath, divs);
+}
+
 function repeat(add, response){
     if(add.more ===true){
         userInput();
     }else{
-        render(response);
+        const divs = render(response);
+        output(divs);
     }
 }
 
@@ -100,34 +104,28 @@ function userInput() {
             .prompt(userSchool)
             .then((add) => {
                 response.school = add.specific;
-                teamMember.push(response);
                 const forIntern = new Intern(response.names, response.id, response.email, response.school);
                 teamMember.push(forIntern);
                 repeat(add,teamMember);
-            })
+            });
         }else{
             inquirer
             .prompt(userOfficeNumber)
             .then((add) => {
                 response.officeNumber = add.specific;
-                teamMember.push(response);
                 const forManager = new Manager(response.names, response.id, response.email, response.officeNumber);
                 teamMember.push(forManager);
                 repeat(add, teamMember);
-            })
+            });
         }
     })
 }
+
 userInput();
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
